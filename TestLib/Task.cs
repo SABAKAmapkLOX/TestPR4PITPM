04c75d6d024ef42cb675993575386fa9c36bc0e1
исп-34 <-34@LAB21-09>
2025-04-14 10:43:37 +0300

    Добавьте файлы проекта.

---
+++ TestLib/Task.cs
@@ -0,0 +1,52 @@
+﻿namespace TestLib
+{
+    public class Task
+    {
+        public static List<int> TaskOne()
+        {
+            List<int> numberList = new List<int>();
+            for (int i = 1000; i <= 9999; i++)
+            {
+                int numberOne = i / 1000;
+                int numberTwo = (i % 1000) / 100;
+                int numberThree = (i % 100) / 10;
+                int numberFour = i % 10;
+                if (numberOne != numberTwo && numberTwo != numberThree && numberThree != numberFour)
+                {
+                    numberList.Add(i);
+                }
+            }
+            return numberList;
+        }
+        public static List<int> TaskTwo()
+        {
+            List<int> numberList = new List<int>();
+            int number = 0;
+            for (int numberOne = 0; numberOne <= 9; numberOne++)
+            {
+                for (int numberTwo = 0; numberTwo <= 9; numberTwo++)
+                {
+                    if (numberOne != numberTwo)
+                    {
+                        for (int numberThree = 0; numberThree <= 9; numberThree++)
+                        {
+                            if (numberThree != numberTwo && numberThree != numberOne)
+                            {
+                                for (int numberFour = 0; numberFour <= 9; numberFour++)
+                                {
+                                    if (numberFour != numberThree && numberThree != numberTwo && numberTwo != numberOne)
+                                    {
+                                        number = numberOne * 1000 + numberTwo * 100 + numberThree * 10 + numberFour;
+                                        numberList.Add(number);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return numberList;
+        }
+    }
+}
---
+++ TestMsTest/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace TestMsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
